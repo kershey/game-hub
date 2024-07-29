43055561fad7933ff3047ba6ba0af6e9885126fa
Jensen Kershey Aragon <jnsnaragon01@gmail.com>
2024-07-30 03:24:14 +0800

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { useState } from 'react';
 import './App.css';
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Button colorScheme='blue'>Button</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
